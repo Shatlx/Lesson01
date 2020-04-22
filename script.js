40b2ed7013679a98bfb36bd46fddea30f3ea8a3c
iShatik <ilshat.khamatdinov@gmail.com>
2020-04-22 23:46:33 +0500

    first commitlog

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 3;
+const pi = 3.14;
+
+console.log(second);
+
+let b = "string";
+
+console.log(b);
+
+//confirm("Вам исполнилось 18 лет?");
+alert("Привет Мир");
